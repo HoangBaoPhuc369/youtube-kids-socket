19cb546fe223954e130df17ce3aab244fd819b47
HoangBaoPhuc369 <hoangbaophuc369@gmail.com>
2022-12-17 23:46:46 +0700

    Fix socket

--- index.js
+++ index.js
@@ -1,86 +1,36 @@
 const express = require("express");
-const socket = require("socket.io");
-const PORT = 8900;
-
 const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
 
-const server = app.listen(PORT, () => {
-  console.log(`server is listening on port ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
-});
+const server = http.createServer(app);
 
-const io = socket(server, {
+const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
-let users = [];
-
-const addUser = (userId, socketId, userName, picture, timeJoin) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId, userName, picture, timeJoin });
-};
-
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
-
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
-
 io.on("connection", (socket) => {
-  //when connect
-  console.log("a user connected.");
+  console.log(`User Connected: ${socket.id}`);
 
-  //take userId and socketId from user
-  socket.on("addUser", (data) => {
-    addUser(data.userId, socket.id);
-    io.emit("getUsers", users);
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
-  //send and get message
-  socket.on("sendMessage", ({ messages, currentChatID }) => {
-    const user = getUser(messages?.receiver);
-    io.to(user?.socketId).emit("getMessage", { messages, currentChatID });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
   });
 
-  //get and sent message delivered
-  socket.on("messageDelivered", (data) => {
-    const user = getUser(data.message?.sender);
-    io.to(user?.socketId).emit("getMessageDelivered", data);
-  });
-
-  //get and sent message seen
-  socket.on("messageSeen", (data) => {
-    const user = getUser(data.message?.sender);
-    io.to(user?.socketId).emit("getMessageSeen", data);
-  });
-
-  //get and sent message seen all
-  socket.on("messageSeenAll", (data) => {
-    const user = getUser(data.receiverId);
-    io.to(user?.socketId).emit("getMessageSeenAll", data);
-  });
-
-  // Listen typing events
-  socket.on("start typing message", (data) => {
-    console.log("start typing message", data);
-    const user = getUser(data.receiverId);
-    io.to(user?.socketId).emit("start typing message", data);
-  });
-  socket.on("stop typing message", (data) => {
-    const user = getUser(data.receiverId);
-    io.to(user?.socketId).emit("stop typing message", data);
-  });
-
-  //when disconnect
   socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
+    console.log("User Disconnected", socket.id);
   });
+});
 
+server.listen(8900, () => {
+  console.log("SERVER RUNNING");
 });
