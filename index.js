b1fbed1c3ebbb310a9e78e24c576f605836d229e
HoangBaoPhuc369 <hoangbaophuc369@gmail.com>
2022-12-22 09:50:23 +0700

    update

--- index.js
+++ index.js
@@ -14,8 +14,29 @@ const io = new Server(server, {
   },
 });
 
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
+
+  //take userId and socketId from user
+  socket.on("addUser", (data) => {
+    addUser(data.userId, socket.id);
+    io.emit("getUsers", users);
+  });
 
   socket.on("join_room", (data) => {
     socket.join(data);
@@ -26,8 +47,15 @@ io.on("connection", (socket) => {
     socket.to(data.room).emit("receive_message", data);
   });
 
+  socket.on("watch_video", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("get_watch_video_activity", data);
+  });
+
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
+    removeUser(socket.id);
+    io.emit("getUsers", users);
   });
 });
 
